0d2276f923d02546ec152f67e25a3cc574c4c84e
Florian <florian.martin63000@gmail.com>
2021-06-12 19:23:53 +0200

    Feat: WIP neumorphism design for mat-card

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,18 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 
-const MATERIALS = [MatButtonModule];
+const MATERIALS = [MatButtonModule, MatCardModule];
 @NgModule({
-  declarations: [AppComponent],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
-    MATERIALS,
-  ],
-  providers: [],
-  bootstrap: [AppComponent],
+    declarations: [AppComponent],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        BrowserAnimationsModule,
+        MATERIALS,
+    ],
+    providers: [],
+    bootstrap: [AppComponent],
 })
 export class AppModule {}
