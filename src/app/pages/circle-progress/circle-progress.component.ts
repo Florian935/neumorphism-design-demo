379e590fbb02502ed7e624f27715df40a4e08734
florian935 <florian.martin63000@gmail.com>
2022-01-10 15:57:18 +0100

    Feat: WIP circular progress

--- src/app/pages/circle-progress/circle-progress.component.ts
+++ src/app/pages/circle-progress/circle-progress.component.ts
@@ -1,24 +1,41 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
 
 @Component({
     selector: 'app-circle-progress',
     templateUrl: './circle-progress.component.html',
     styleUrls: ['./circle-progress.component.scss'],
 })
-export class CircleProgressComponent {
+export class CircleProgressComponent implements AfterViewInit {
     @ViewChild('circle') circle: any;
-    input = 0;
-    pourcentage = 0;
+    percent = 0;
+
+    ngAfterViewInit(): void {
+        this.circle.nativeElement.style.strokeDasharray = '890.49';
+        this.circle.nativeElement.style.strokeWidth = '0';
+    }
 
     constructor() {}
 
-    changePercent(): void {
-        let change = 870.49 - (870.49 * this.input) / 100;
-        this.pourcentage = this.input;
-        this.circle.nativeElement.style.strokeDashoffset = change.toString();
-    }
+    onChangePercent(percent: string): void {
+        this.percent = parseInt(percent);
 
-    onKeyUp(event: any): void {
-        this.input = event.value;
+        if (percent === '100') {
+            this.circle.nativeElement.style.strokeDasharray = '0';
+
+            return;
+        }
+
+        if (percent === '0' || !percent) {
+            this.circle.nativeElement.style.strokeWidth = '0';
+            this.percent = 0;
+            return;
+        }
+
+        this.circle.nativeElement.style.strokeDasharray = '890.49';
+
+        this.circle.nativeElement.style.strokeWidth = '24';
+        const strokeDasharray = this.circle.nativeElement.style.strokeDasharray;
+        const change = strokeDasharray - (strokeDasharray * this.percent) / 100;
+        this.circle.nativeElement.style.strokeDashoffset = change.toString();
     }
 }
